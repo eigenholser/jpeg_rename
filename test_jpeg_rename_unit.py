43593559dd30acd1507edb4741d013cfaed46295
Scott Overholser <s@eigenholser.com>
2014-09-01 12:11:59 -0600

    Adds new tests.

--- test_jpeg_rename_unit.py
+++ test_jpeg_rename_unit.py
@@ -22,7 +22,7 @@ def test_get_new_fn_with_invalid_exif_data():
 
     exif_data = {'DateTimeOriginal': '2014:08:16 06:20'}
     old_fn = 'abc123.jpg'
-    filemap = FileMap(old_fn, exif_data)
+    filemap = FileMap(old_fn, None, exif_data)
     new_fn = filemap.new_fn
     assert old_fn == new_fn
 
@@ -32,7 +32,7 @@ def test_get_new_fn_with_no_exif_data():
     extension."""
     exif_data = {}
     old_fn = 'abc123.jpg'
-    filemap = FileMap(old_fn, exif_data)
+    filemap = FileMap(old_fn, None, exif_data)
     new_fn = filemap.new_fn
     assert old_fn == new_fn
 
@@ -41,7 +41,7 @@ def test_get_new_fn_with_exif_data_and_wrong_ext():
     """Test get_new_fn() with valid EXIF data and wrong file extension."""
     exif_data = {'DateTimeOriginal': '2014:08:16 06:20:20'}
     old_fn = 'abc123.jpeg'
-    filemap = FileMap(old_fn, exif_data)
+    filemap = FileMap(old_fn, None, exif_data)
     new_fn = filemap.new_fn
     assert new_fn == '20140816_062020.jpg'
 
@@ -65,6 +65,7 @@ def test_get_exif_data(mock_img, mock_tags):
     assert filemap.exif_data == {'DateTimeOriginal': '2014:08:16 06:20:20'}
 
 
+#@pytest.mark.skipif('True', reason="Work in progress")
 @patch('jpeg_rename.TAGS')
 @patch.object(Image, 'open')
 def test_get_exif_data_info_none(mock_img, mock_tags):
@@ -85,6 +86,83 @@ def test_get_exif_data_info_none(mock_img, mock_tags):
 
     assert excinfo.value[0] == "{0} has no EXIF data.".format(old_fn)
 
+@patch('jpeg_rename.FileMap.make_new_fn_unique')
+@patch('jpeg_rename.os.rename')
+def test_move_orthodox(mock_os, mock_fn_unique):
+    """Rename file with mocked os.rename. Verify called with args."""
+    mock_fn_unique.return_value = None
+    old_fn = 'abc123.jpg'
+    exif_data = {'DateTimeOriginal': '2014:08:16 06:20'}
+    filemap = FileMap(old_fn, avoid_collisions=None, exif_data=exif_data)
+    new_fn = filemap.new_fn
+    filemap.move()
+    mock_os.assert_called_with(old_fn, new_fn)
+
+@patch('jpeg_rename.FileMap.make_new_fn_unique')
+@patch('jpeg_rename.os.path.exists')
+def test_move_collision_detected(mock_exists, mock_fn_unique):
+    """Move file with collision_detected simulating avoid_collisions = False.
+    """
+    mock_fn_unique.return_value = None
+    mock_exists.return_value = True
+    old_fn = 'abc123.jpg'
+    exif_data = {}
+    filemap = FileMap(old_fn, avoid_collisions=None, exif_data=exif_data)
+    filemap.collision_detected = True
+    new_fn = filemap.new_fn
+    filemap.move()
+    assert new_fn == old_fn
+
+@patch('jpeg_rename.os.path.exists')
+def test_rename_empty_exif_data(mock_exists):
+    """Make unique filename with empty EXIF data."""
+    mock_exists.return_value = True
+    old_fn = 'abc123.jpg'
+    exif_data = {'DateTimeOriginal': '2014:08:16 06:20'}
+    filemap = FileMap(old_fn, avoid_collisions=True, exif_data=exif_data)
+    new_fn = filemap.new_fn
+    filemap.make_new_fn_unique()
+    assert filemap.new_fn == old_fn
+
+@patch('jpeg_rename.os.path.exists')
+def test_rename_with_valid_exif_data_and_avoid_collisions(mock_exists):
+    """Make unique new filename from valid EXIF data. Avoid collisions."""
+    mock_exists.return_value = True
+    old_fn = 'abc123.jpg'
+    exif_data = {'DateTimeOriginal': '2014:08:16 06:20:20'}
+    filemap = FileMap(old_fn, avoid_collisions=True, exif_data=exif_data)
+    new_fn = filemap.new_fn
+    filemap.MAX_RENAME_ATTEMPTS = 2
+    with pytest.raises(Exception):
+        filemap.make_new_fn_unique()
+    assert filemap.new_fn == '20140816_062020.jpg'
+
+@patch('jpeg_rename.os.path.exists')
+def test_rename_with_valid_exif_data_and_no_avoid_collisions(mock_exists):
+    """Make unique new filename from valid EXIF data. Do not avoid collisions.
+    """
+    mock_exists.return_value = True
+    old_fn = 'abc123.jpg'
+    exif_data = {'DateTimeOriginal': '2014:08:16 06:20:20'}
+    filemap = FileMap(old_fn, avoid_collisions=False, exif_data=exif_data)
+    new_fn = filemap.new_fn
+    filemap.MAX_RENAME_ATTEMPTS = 2
+    filemap.make_new_fn_unique()
+    assert filemap.new_fn == '20140816_062020.jpg'
+
+@patch('jpeg_rename.os.path.exists')
+def test_rename_no_collision(mock_exists):
+    """Make unique new filename from valid EXIF data. Do not avoid collisions.
+    """
+    mock_exists.return_value = False
+    old_fn = 'abc123.jpg'
+    exif_data = {}
+    filemap = FileMap(old_fn, avoid_collisions=False, exif_data=exif_data)
+    new_fn = filemap.new_fn
+    filemap.make_new_fn_unique()
+    assert filemap.new_fn == old_fn
+
+#@pytest.mark.skipif('True', reason="Work in progress")
 
 class TestFileMap():
 
