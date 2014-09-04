daf1444ea6df6fc78285d38da33f20b696be497d
Scott Overholser <s@eigenholser.com>
2014-09-03 20:44:14 -0600

    Completes tests with valid asserts.

--- test_jpeg_rename_unit.py
+++ test_jpeg_rename_unit.py
@@ -245,70 +245,91 @@ def test_process_file_map_simon_no_sez(mock_fm):
     mock_fm.new_fn = 'abc123.jpg'
     file_map = [mock_fm, mock_fm]
     process_file_map(file_map, simon_sez=None)
-    #assert
+    # TODO: assert
 
 #@pytest.mark.skipif('True', reason="Work in progress")
+@patch('jpeg_rename.init_file_map')
 @patch('jpeg_rename.os.access')
 @patch('jpeg_rename.os.path.dirname')
 @patch('jpeg_rename.os.path.exists')
 @patch('jpeg_rename.os.path.abspath')
-def test_process_all_files_workdir_none(mock_abspath, mock_exists, mock_dirname, mock_os_access):
-    """
-    """
+def test_process_all_files_workdir_none(mock_abspath, mock_exists,
+        mock_dirname, mock_os_access, mock_init_file_map):
+    """Test process_all_files() with workdir=None, avoid_collisions=None.
+    Verify that init_file_map() is called with (DIRNAME, None)."""
     DIRNAME = '/foo/bar'
     mock_abspath.return_value = DIRNAME
     mock_exists.return_value = True
     mock_dirname.return_value = DIRNAME
     process_all_files()
-    mock_dirname.assert_called_with(DIRNAME)
+    mock_init_file_map.assert_called_with(DIRNAME, None)
 
 #@pytest.mark.skipif('True', reason="Work in progress")
+@patch('jpeg_rename.process_file_map')
+@patch('jpeg_rename.init_file_map')
 @patch('jpeg_rename.os.access')
-def test_process_all_files_workdir_not_none(mock_os_access):
+def test_process_all_files_workdir_not_none(mock_os_access, mock_init_file_map,
+        mock_process_file_map):
+    """Test process_all_files() with workdir set. Tests negative of branch
+    testing workdir. Verify process_file_map() called with expected arguments.
     """
-    """
+    file_map = [TestFileMap()]
+    mock_init_file_map.return_value = file_map
     mock_os_access.return_value = True
     process_all_files('.')
-    # assert
+    mock_process_file_map.assert_called_with(file_map, None)
 
 #@pytest.mark.skipif('True', reason="Work in progress")
-@patch('jpeg_rename.os.access')
-def test_process_all_files_access_true(mock_os_access):
-    """
-    """
-    mock_os_access.return_value = True
-    process_all_files('.')
-    # assert
-
-#@pytest.mark.skipif('True', reason="Work in progress")
-@patch('jpeg_rename.os.access')
-@patch('jpeg_rename.sys.exit')
-def test_process_all_files_access_false(mock_sys_exit, mock_os_access):
-    """
-    """
-    mock_os_access.return_value = False
-    process_all_files('.')
-    # assert
-    mock_sys_exit.assert_called_with(1)
-
-#@pytest.mark.skipif('True', reason="Work in progress")
+@patch('jpeg_rename.process_file_map')
+@patch('jpeg_rename.init_file_map')
 @patch('jpeg_rename.os.path.exists')
-def test_process_all_files_exists_true(mock_os_path):
-    """
-    """
+def test_process_all_files_exists_true(mock_os_path, mock_init_file_map,
+        mock_process_file_map):
+    """Test process_all_files() with workdir path exists True. Verify that
+    process_file_map() called with correct arguments."""
+    file_map = [TestFileMap()]
+    mock_init_file_map.return_value = file_map
     mock_os_path.return_value = True
     process_all_files('.')
-    # assert
+    mock_process_file_map.assert_called_with(file_map, None)
 
 #@pytest.mark.skipif('True', reason="Work in progress")
 @patch('jpeg_rename.os.path.exists')
 @patch('jpeg_rename.sys.exit')
 def test_process_all_files_exists_false(mock_sys_exit, mock_os_path):
-    """
-    """
+    """Test process_all_files() with workdir path exists False. Tests positive
+    branch of workdir not exists test. Verify that sys.exit() is called with
+    expected argument."""
     mock_os_path.return_value = False
     process_all_files('.')
     mock_sys_exit.assert_called_with(1)
+
+#@pytest.mark.skipif('True', reason="Work in progress")
+@patch('jpeg_rename.process_file_map')
+@patch('jpeg_rename.init_file_map')
+@patch('jpeg_rename.os.access')
+def test_process_all_files_access_true(mock_os_access, mock_init_file_map,
+        mock_process_file_map):
+    """Test process_all_files() with workdir access True. Tests for negative
+    branch of W_OK access test. Verify process_file_map() called with expected
+    arguments."""
+    file_map = [TestFileMap()]
+    mock_init_file_map.return_value = file_map
+    mock_os_access.return_value = True
+    process_all_files('.')
+    mock_process_file_map.assert_called_with(file_map, None)
+
+#@pytest.mark.skipif('True', reason="Work in progress")
+@patch('jpeg_rename.os.access')
+@patch('jpeg_rename.sys.exit')
+def test_process_all_files_access_false(mock_sys_exit, mock_os_access):
+    """Test process_all_files() with workdir access False. Tests for positive
+    branch of W_OK access test. Verify sys.exit() called with expected
+    arguments."""
+    mock_os_access.return_value = False
+    process_all_files('.')
+    mock_sys_exit.assert_called_with(1)
+
 
 class TestFileMap():
 
