d02c6af838d37c496de81399e446c43cff3f14cb
Scott Overholser <eigenholser@users.noreply.github.com>
2017-02-26 10:04:28 -0700

    Organize unit tests.

--- tests/test_move.py
+++ tests/test_move.py
@@ -9,63 +9,6 @@ from photo_rename import *
 from stubs import *
 
 
-class TestReadExifData():
-    """Tests for method read_exif_data() are in this class."""
-    @pytest.mark.skipif(RUN_TEST, reason="Work in progress")
-    @patch.object(pyexiv2, 'ImageMetadata')
-    def test_read_exif_data(self, mock_img_md):
-        """Tests read_exif_data() with valid EXIF data. Tests for normal
-        operation. Verify expected EXIF data in instantiated object."""
-
-        class StubExifTag(object):
-            raw_value = EXIF_DATA_VALID['exif_data']['Exif.Image.DateTime']
-
-        class TestImage(object):
-            """
-            ImageMetadata test stub.
-            """
-            def __getitem__(self, key):
-                return StubExifTag()
-
-            def read(self):
-                return EXIF_DATA_VALID['exif_data']
-
-            @property
-            def exif_keys(self):
-                return ['Exif.Image.DateTime']
-
-
-        old_fn = OLD_FN_JPG_LOWER
-        mock_img_md.return_value = TestImage()
-        filemap = FileMap(old_fn, IMAGE_TYPE_JPEG)
-        assert filemap.metadata == EXIF_DATA_VALID['exif_data']
-
-    @pytest.mark.skipif(RUN_TEST, reason="Work in progress")
-    @patch.object(pyexiv2, 'ImageMetadata')
-    def test_read_exif_data_info_none(self, mock_img_md):
-        """
-        Tests read_exif_data() with no EXIF data available. Tests for raised
-        Exception. Verify expected exception message.
-        """
-
-        class TestImage(object):
-            """
-            ImageMetadata test stub.
-            """
-            def read(self):
-                return None
-
-            @property
-            def exif_keys(self):
-                return []
-
-        old_fn = OLD_FN_JPG_LOWER
-        mock_img_md.return_value = TestImage()
-        with pytest.raises(Exception) as excinfo:
-            filemap = FileMap(old_fn, IMAGE_TYPE_JPEG)
-        assert str(excinfo.value) == "{0} has no EXIF data.".format(old_fn)
-
-
 class TestMove():
     """
     Test for method move() are in this class.
