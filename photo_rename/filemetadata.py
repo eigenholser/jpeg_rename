54728ed1d20ed703f424015838a8e3732be454df
Scott Overholser <eigenholser@users.noreply.github.com>
2017-05-06 16:47:48 -0600

    Cleanup

--- photo_rename/filemetadata.py
+++ photo_rename/filemetadata.py
@@ -15,16 +15,14 @@ logger = logging.getLogger(__name__)
 @photo_rename.logged_class
 class FileMetadata(object):
     """
+    Contains logic to perform metadata operations on file.
     """
 
     def __init__(self, file):
-        """
-        Object that reads, writes, and stores EXIF and XMP metadata from file.
-        """
         self.file = file
+        self.metadata = None
         self.img_md = pyexiv2.ImageMetadata("{}".format(file))
         self.img_md.read()
-        self.metadata = None
 
     def __getitem__(self, key):
         """
@@ -39,9 +37,6 @@ class FileMetadata(object):
         """
         Read EXIF or XMP data from file. Convert to Python dict.
         """
-        # Xmp.xmp.CreateDate
-        # Exif.Image.DateTime
-
         metadata = {}
 
         xmp_keys = [md_key for md_key in self.img_md.xmp_keys]
@@ -56,24 +51,21 @@ class FileMetadata(object):
     def set_datetime(self, new_datetime):
         """
         Update EXIF/XMP tags as needed and write metadata.
-        TODO: Test writeable on each file prior to attempt. Not here though.
-        TODO: Timezone?
         """
-        # Xmp.xmp.MetadataDate : 2017-03-03T08:07:01-07:00
-        # Xmp.xmp.CreateDate : 2017-02-27T15:19:31-07:00
-        # Exif.Image.DateTime : 2017:03:03 09:27:53
-        # Xmp.xmp.ModifyDate : 2017-03-03T09:27:53-07:00
+        # TODO: Timezone?
         xmp_datetime = new_datetime.strftime('%Y-%m-%dT%H:%M:%S')
         exif_datetime = new_datetime.strftime('%Y:%m:%d %H:%M:%S')
         logger.debug("XMP: {} : EXIF: {}".format(xmp_datetime, exif_datetime))
 
-        # Shotwell requires Xmp.xmp.CreateDate to be set.
-        self.img_md['Xmp.xmp.CreateDate'] = pyexiv2.XmpTag(
-                'Xmp.xmp.CreateDate', new_datetime)
+        # Shotwell will use Xmp.xmp.CreateDate if set, so it must be updated.
+        if 'Xmp.xmp.CreateDate' in self['metadata'].keys():
+            self.img_md['Xmp.xmp.CreateDate'] = pyexiv2.XmpTag(
+                    'Xmp.xmp.CreateDate', new_datetime)
+        if 'Exif.Photo.DateTimeOriginal' in self['metadata'].keys():
+            self.img_md['Exif.Photo.DateTimeOriginal'] = pyexiv2.ExifTag(
+                    'Exif.Photo.DateTimeOriginal', new_datetime)
         self.img_md['Exif.Image.DateTime'] = pyexiv2.ExifTag(
                 'Exif.Image.DateTime', new_datetime)
-        self.img_md['Exif.Photo.DateTimeOriginal'] = pyexiv2.ExifTag(
-                'Exif.Photo.DateTimeOriginal', new_datetime)
 
         try:
             self.img_md.write()
