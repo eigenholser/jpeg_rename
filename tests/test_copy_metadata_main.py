e94a3d3b4d3a6eabf0ba0f3a1a39eb5a063c2d07
Scott Overholser <eigenholser@users.noreply.github.com>
2017-05-24 06:01:41 -0600

    Add tests for copy_metadata main() method.

--- tests/test_copy_metadata_main.py
+++ tests/test_copy_metadata_main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import sys
 import pytest
@@ -17,23 +18,36 @@ class TestCopyMetadataMain(object):
     skiptests = not TEST_COPY_METADATA_MAIN
 
     @pytest.mark.skipif(skiptests, reason="Work in progress")
+    @pytest.mark.parametrize(
+        "verbose, log_level", [
+            (False, logging.INFO,),
+            (True, logging.DEBUG,),
+    ])
     @patch('photo_rename.copy_metadata.CustomArgumentParser')
     @patch('photo_rename.copy_metadata.process_all_files')
     @patch('photo_rename.copy_metadata.os.path.isdir')
     @patch('photo_rename.copy_metadata.os.path.exists')
-    def test_copy_metadata_main_function(self,
-            m_exists, m_isdir, m_process_all_files, m_argparser):
+    @patch('photo_rename.copy_metadata.logging')
+    def test_copy_metadata_main_args_with_verbose_params(self,
+            m_logging, m_exists, m_isdir, m_process_all_files, m_argparser,
+            verbose, log_level):
         """
         Test main() function. Mock CustomArgumentParser to return values
-        desired for test. Verify process_all_files called with expected
-        arguments.
+        desired for test. Parametrize different combinations of verbosity
+        and log level. Verify that logging configured properly for verbose
+        setting.
         """
         m_exists.return_value = True
         m_isdir.return_value = True
 
         # Mock up the proper return values.
         m_parse_args = MagicMock(
-                src_directory="/abc", dst_directory="/def", simon_sez=True)
+            src_directory="/abc",
+            dst_directory="/def",
+            simon_sez=True,
+            verbose=verbose,
+        )
+
         attrs = {
             'parse_args.return_value': m_parse_args,
         }
@@ -43,6 +57,96 @@ class TestCopyMetadataMain(object):
         m_parse_args_container.configure_mock(**attrs)
         m_argparser.return_value = m_parse_args_container
 
+        # Invoke the unit.
         retval = main()
+
+        # Confirm expected behavior
+        m_logging.basicConfig.called_once_with(level=log_level)
         m_process_all_files.assert_called_with('/abc', '/def', simon_sez=True)
+
+
+    # TODO: Check the m_exit mock. Is it useful? Something wrong with this.
+    @pytest.mark.skipif(skiptests, reason="Work in progress")
+    @patch('photo_rename.copy_metadata.CustomArgumentParser')
+    @patch('photo_rename.copy_metadata.process_all_files')
+    @patch('photo_rename.copy_metadata.os.path.isdir')
+    @patch('photo_rename.copy_metadata.os.path.exists')
+    @patch('photo_rename.copy_metadata.os.sys.exit')
+    @patch('photo_rename.copy_metadata.logger')
+    def test_copy_metadata_main_missing_directory(self, m_logger, m_exit,
+            m_exists, m_isdir, m_process_all_files, m_argparser):
+        """
+        Test main() function. Mock CustomArgumentParser to return values
+        desired for test but with missing `src_directory`. Verify correct
+        behavior by asserting calls on logger, usage_message, and exit.
+        """
+        m_exists.return_value = True
+        m_isdir.return_value = True
+
+        # Mock up the proper return values.
+        m_parse_args = MagicMock(
+            src_directory=None,
+            dst_directory="/def",
+            simon_sez=True,
+            verbose=False,
+        )
+
+        attrs = {
+            'parse_args.return_value': m_parse_args,
+            'usage_message.return_value': None,
+        }
+
+        # This one configured to return m_parse_args.
+        m_parse_args_container = MagicMock()
+        m_parse_args_container.configure_mock(**attrs)
+        m_argparser.return_value = m_parse_args_container
+
+        # Invoke the unit.
+        retval = main()
+
+        # Confirm expected behavior
+        m_logger.error.assert_called()
+        m_parse_args_container.usage_message.assert_called_once()
+        m_exit.assert_called_once_with(1)
+
+    @pytest.mark.skipif(skiptests, reason="Work in progress")
+    @patch('photo_rename.copy_metadata.CustomArgumentParser')
+    @patch('photo_rename.copy_metadata.process_all_files')
+    @patch('photo_rename.copy_metadata.os.path.isdir')
+    @patch('photo_rename.copy_metadata.os.path.exists')
+    @patch('photo_rename.copy_metadata.os.sys.exit')
+    def test_copy_metadata_main_path_not_exist(self, m_exit, m_exists,
+            m_isdir, m_process_all_files, m_argparser):
+        """
+        Test main() function. Mock CustomArgumentParser to return values
+        desired for test. Mock os.path.exists to return False. Verify
+        usage_message() called once.
+        """
+        m_exists.return_value = False
+        m_isdir.return_value = True
+
+        # Mock up the proper return values.
+        m_parse_args = MagicMock(
+            src_directory="/abc",
+            dst_directory="/def",
+            simon_sez=True,
+            verbose=False,
+        )
+
+        attrs = {
+            'parse_args.return_value': m_parse_args,
+            'usage_message.return_value': None,
+        }
+
+        # This one configured to return m_parse_args.
+        m_parse_args_container = MagicMock()
+        m_parse_args_container.configure_mock(**attrs)
+        m_argparser.return_value = m_parse_args_container
+
+        # Invoke the unit.
+        retval = main()
+
+        # Confirm expected behavior
+        m_exit.assert_called_once_with(1)
+        m_parse_args_container.usage_message.assert_called_once()
 
